82cd88559d337ce9cfc12c8335b833c9964f0266
Marcus <marcus.vinicius.bittencourt.c@gmail.com>
2024-12-28 20:00:57 -0300

    mudanças de tamanho de texto de novo

--- app/animateless/page.tsx
+++ app/animateless/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import 'animate.css';
 
 const buttonStyle =
-    'text-white text-2xl sm:text-3xl border border-indigo-950 rounded bg-slate-600 py-2 px-8 w-full';
+    'text-white text-2xl border border-indigo-950 rounded bg-slate-600 py-2 px-8 w-full';
 
 const page = () => {
     // adicionar classes de animação, esperar a animação terminar e remover a classe
@@ -160,17 +160,17 @@ const page = () => {
                     Animate.css
                 </h1>
             </div>
-            <div className="flex flex-wrap w-full justify-around items-center gap-4">
-                <div className="flex flex-col items-center gap-4 w-[80%]">
+            <div className="flex flex-wrap w-full justify-around gap-4">
+                <div className="flex flex-col items-center gap-4 w-[80%] sm:w-[20%]">
                     {botoes(...primeiroQuarto)}
                 </div>
-                <div className="flex flex-col items-center gap-4 w-[80%]">
+                <div className="flex flex-col items-center gap-4 w-[80%] sm:w-[20%]">
                     {botoes(...segundoQuarto)}
                 </div>
-                <div className="flex flex-col items-center gap-4 w-[80%]">
+                <div className="flex flex-col items-center gap-4 w-[80%] sm:w-[20%]">
                     {botoes(...terceiroQuarto)}
                 </div>
-                <div className="flex flex-col items-center gap-4 w-[80%]">
+                <div className="flex flex-col items-center gap-4 w-[80%] sm:w-[20%]">
                     {botoes(...quartoQuarto)}
                 </div>
             </div>
